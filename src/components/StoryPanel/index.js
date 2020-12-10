f7fe4aa7cd111fefd931c9952af82fb873fbe270
Dale Shlass <dshlass@freedommobile.ca>
2020-12-10 13:26:50 -0500

    save point.

--- src/components/StoryPanel/index.js
+++ src/components/StoryPanel/index.js
@@ -4,23 +4,29 @@ const StoryPanel = () =>  {
 
   const [text, setText] = useState('');
   const [image, setImage] = useState(null);
+  const [reverse, setReverse] = useState(false);
 
   const handleImage = (event) =>
     setImage(URL.createObjectURL(event.target.files[0]));
   
   const handleText =(e) => {
     const {value} = e.currentTarget
-
     setText(value)
+  }
+
+  const handleReverse = (e) => {
+    const {checked} = e.currentTarget
+    setReverse(checked)
+  }
+
+  const handleAddText = () => {
+    
   }
 
   return (
     <div className="storyPanel">
       <div>
         <div className="storyPanel__input storyPanel__input-text">
-          <label htmlFor="photo">Add Story Text</label>
-        </div>
-        <div className="storyPanel__input storyPanel__input-image">
           <label htmlFor="photo">Upload New Photo</label>
           <input
             type="file"
@@ -31,12 +37,15 @@ const StoryPanel = () =>  {
             onInput={(event) => handleImage(event)}
           />
         </div>
+        <div className="storyPanel__input">
+          <label htmlFor="photo">Reverse Image and Text</label>
+          <input type="checkbox" checked={reverse} value={reverse} onChange={(e) => handleReverse(e)}/>
+        </div>
       </div>
 
-      <div className="storyPanel__container">
+      <div className="storyPanel__container" style={{flexDirection: reverse ? 'row-reverse' : 'row'}}>
         <div className="storyPanel__text-container">
-            {/* <p className="storyPanel__text">{text}</p> */}
-          <textarea value={text} onChange={(e) => handleText(e)}/>
+          <textarea value={text} rows="20" cols="40" placeholder={"Please enter in story text"}  onChange={(e) => handleText(e)}/>
         </div>
         <div className="storyPanel__image-container">
           <img className="storyPanel__image" src={image} alt="Please upload a background..." />
