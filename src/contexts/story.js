84149a0d2aea595612032e7929091c3b95c802bc
Dale Shlass <dshlass@freedommobile.ca>
2020-12-10 14:47:17 -0500

    removed storyPanel.

--- src/contexts/story.js
+++ src/contexts/story.js
@@ -47,7 +47,7 @@ const reducer = (state, action) => {
 
 export const StoryProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, mockStories);
-
+  console.log(state)
   return (
     <StoryContextState.Provider value={state}>
       <StoryContextDispatch.Provider value={dispatch}>
