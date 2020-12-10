94ef64934cc25e5090ee3d835fa8ccdf3e0d9e61
Jonathan Wong <jonathan.wong@applydigital.co>
2020-12-10 16:22:31 -0500

    working on homepage

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <section className="homepage__body">Home Page</section>;
+  return (
+    <section className="homepage__body">
+      <h2>Welcome to Reel Science!</h2>
+      <div>
+        <Link to="/createStory" className="homepage__link">
+          Create a Story
+        </Link>
+      </div>
+      <div>
+        <Link to="/readStory" className="homepage__link">
+          View Stories
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
